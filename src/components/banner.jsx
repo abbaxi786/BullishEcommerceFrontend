ab40b19ccc38d2d095b11d0641e82516d21011d8
abbaxi786 <141753795+abbaxi786@users.noreply.github.com>
2026-05-08 20:09:53 +0500

    the banner setting

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -7,8 +7,10 @@ function Banner() {
   return (
     <div className="container-fluid p-0">
 
+      {/* ================= MAIN BANNER ================= */}
+
       <div
-        className="d-flex justify-content-center align-items-center bannerSetting"
+        className="d-flex justify-content-center align-items-center bannerSetting w-100"
         style={{
           height: "450px",
           cursor: "pointer",
@@ -19,55 +21,58 @@ function Banner() {
         onClick={() => navigate("/mencollection")}
       />
 
-      <div className="row g-0">
+      {/* ================= CATEGORY SECTION ================= */}
 
-        <div
-          className="col-6 sizeUp col-md-3 d-flex justify-content-center align-items-center"
-          style={cardStyle}
-          onClick={() => navigate("/category/69e2109e78e59f607d72c33a")}
-        >
-          <img src="/images/jacket.png" alt="jacket" style={imgStyle} />
-        </div>
+      <div className="container-fluid px-0">
 
-        <div
-          className="col-6 sizeUp col-md-3 d-flex justify-content-center align-items-center"
-          style={cardStyle}
-          onClick={() => navigate("/category/69e2109e78e59f607d72c33a")}
-        >
-          <img src="/images/shoes.png" alt="shoes" style={imgStyle} />
-        </div>
+        <div className="row g-0">
 
-        <div
-          className="col-6 sizeUp col-md-3 d-flex justify-content-center align-items-center"
-          style={cardStyle}
-          onClick={() => navigate("/category/69e2105d78e59f607d72c337")}
-        >
-          <img src="/images/watch.png" alt="watch" style={imgStyle} />
-        </div>
+          {/* CARD 1 */}
+          <div
+            className="col-6 col-md-3 categoryTile"
+            onClick={() =>
+              navigate("/category/69e2109e78e59f607d72c33a")
+            }
+          >
+            <img src="/images/jacket.png" alt="jacket" />
+          </div>
 
-        <div
-          className="col-6 sizeUp col-md-3 d-flex justify-content-center align-items-center"
-          style={cardStyle}
-          onClick={() => navigate("/category/69e2108478e59f607d72c338")}
-        >
-          <img src="/images/shirt.png" alt="shirt" style={imgStyle} />
+          {/* CARD 2 */}
+          <div
+            className="col-6 col-md-3 categoryTile"
+            onClick={() =>
+              navigate("/category/69e2109e78e59f607d72c33a")
+            }
+          >
+            <img src="/images/shoes.png" alt="shoes" />
+          </div>
+
+          {/* CARD 3 */}
+          <div
+            className="col-6 col-md-3 categoryTile"
+            onClick={() =>
+              navigate("/category/69e2105d78e59f607d72c337")
+            }
+          >
+            <img src="/images/watch.png" alt="watch" />
+          </div>
+
+          {/* CARD 4 */}
+          <div
+            className="col-6 col-md-3 categoryTile"
+            onClick={() =>
+              navigate("/category/69e2108478e59f607d72c338")
+            }
+          >
+            <img src="/images/shirt.png" alt="shirt" />
+          </div>
+
         </div>
 
       </div>
+
     </div>
   );
 }
-
-const cardStyle = {
-  height: "230px",
-  cursor: "pointer",
-  backgroundColor: "#fff",
-};
-
-const imgStyle = {
-  maxHeight: "90%",
-  maxWidth: "90%",
-  objectFit: "contain",
-};
 
 export default Banner;
